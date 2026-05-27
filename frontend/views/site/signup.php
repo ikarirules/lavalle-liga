8cb8d7d4bb67fac3b3e73c6b3c91aff22084a563
david rodrigo diaz <32314893+ikarirules@users.noreply.github.com>
2026-05-27 11:13:50 -0300

    habilito captcha
    vuelvo a habilitar el captcha

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -26,12 +26,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?php /* CAPTCHA DESHABILITADO TEMPORALMENTE
-                echo $form->field($model, 'verifyCode')->widget(Captcha::class, [
+                <?= $form->field($model, 'verifyCode')->widget(Captcha::class, [
                     'captchaAction' => '/site/captcha',
                     'template'      => '<div class="d-flex align-items-center gap-3 mb-1">{image}</div>{input}',
-                ])->label('Código de verificación');
-                */ ?>
+                ])->label('Código de verificación') ?>
 
                 <div class="form-group">
                     <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
